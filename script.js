60354621a100bf7df4514ac8cf23abc47b359835
ali01554589813-del <ali01554589813@gmail.com>
2026-06-02 23:12:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function addToCart(name, price){
 
     saveCart();
     renderCart();
+    updateCartCount(); // 👈 مهم
     showToast("تمت الإضافة 🛒");
 }
 
@@ -110,5 +111,92 @@ function showToast(msg){
         t.style.display = "none";
     },2000);
 }
+function renderCart(){
 
-renderCart();
+    let box = document.getElementById("cartItems");
+    if(!box) return;
+
+    box.innerHTML = "";
+
+    let total = 0;
+
+    cart.forEach((item,i)=>{
+
+        total += item.price * item.quantity;
+
+        box.innerHTML += `
+        <div class="cart-item">
+            <h4>${item.name}</h4>
+            <p>${item.price} × ${item.quantity}</p>
+            
+            <button onclick="increase(${i})">+</button>
+            <button onclick="decrease(${i})">-</button>
+            <button onclick="removeItem(${i})">حذف</button>
+        </div>
+        `;
+    });
+
+    let t = document.getElementById("total");
+    if(t) t.innerText = "الإجمالي: " + total + " EGP";
+}
+function increase(i){
+    cart[i].quantity++;
+    saveCart();
+    renderCart();
+    updateCartCount();
+}
+
+function decrease(i){
+    if(cart[i].quantity > 1){
+        cart[i].quantity--;
+    } else {
+        cart.splice(i,1);
+    }
+    saveCart();
+    renderCart();
+    updateCartCount();
+}
+function placeOrder(){
+
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let address = document.getElementById("address").value;
+
+    if(name === "" || phone === "" || address === ""){
+        alert("من فضلك املأ جميع البيانات");
+        return;
+    }
+
+    let total = 0;
+    let message = "🛒 طلب جديد من المتجر:%0A%0A";
+
+    cart.forEach(item=>{
+        total += item.price * item.quantity;
+        message += `- ${item.name} × ${item.quantity} = ${item.price * item.quantity} EGP%0A`;
+    });
+
+    message += `%0A👤 الاسم: ${name}`;
+    message += `%0A📞 الهاتف: ${phone}`;
+    message += `%0A📍 العنوان: ${address}`;
+    message += `%0A💰 الإجمالي: ${total} EGP`;
+
+    // رقم واتساب (غيره برقمك)
+    let whatsappNumber = "2001273735156/2001001342050";
+
+    let url = "https://wa.me/" + whatsappNumber + "?text=" + message;
+
+    // فتح واتساب
+    window.open(url, "_blank");
+
+    // تفريغ السلة بعد الإرسال
+    cart = [];
+    localStorage.setItem("cart", JSON.stringify(cart));
+    updateCartCount();
+
+    setTimeout(()=>{
+        window.location.href = "index.html";
+    }, 2000);
+}
+
+
+}updateCartCount();
